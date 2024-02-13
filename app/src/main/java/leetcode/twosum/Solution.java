e25ca81b93cf15c62317111e1544c145f9f69e11
Eugene Yamenko <yamenko@amazon.com>
2024-02-13 15:16:49 +1100

    Minor optimization to avoid unnecessary hash map resizes

--- app/src/main/java/leetcode/twosum/Solution.java
+++ app/src/main/java/leetcode/twosum/Solution.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
  */
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> diffs = new HashMap<>();
+        HashMap<Integer, Integer> diffs = new HashMap<>(nums.length);
 
         for (int i = 0; i < nums.length; i++) {
             int diff = target - nums[i];
